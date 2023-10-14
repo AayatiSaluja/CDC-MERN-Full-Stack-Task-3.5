8a8516b218c3f0d826a7f0f0ac5d46f978f93102
AayatiSaluja <aayati.saluja@gmail.com>
2023-10-14 18:31:10 +0530

    first commit

---
+++ src/components/AgeResult.js
@@ -0,0 +1,21 @@
+import './styling.css';
+import PropTypes from 'prop-types';
+
+const AgeResult = ({ age }) => {
+  return (
+    <h4 className='mt-10 underline text-2xl font-bold dark:text-white'>
+      {/* You are {age.years} years, {age.months} months, and {age.days} days old. */}
+      You are {age.years} years old
+    </h4>
+  );
+};
+
+AgeResult.propTypes = {
+  age: PropTypes.shape({
+    years: PropTypes.number.isRequired,
+    months: PropTypes.number.isRequired,
+    days: PropTypes.number.isRequired,
+  }),
+};
+
+export default AgeResult;
